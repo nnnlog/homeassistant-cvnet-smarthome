7513a4eeb45804a0a1db87785dd83be250915b13
nnnlog <sorisem4106@naver.com>
2025-07-04 17:30:01 +0900

    Replace fan control feature from preset to fan speed

--- custom_components/cvnet/lib/homeassistant/entity/cvnet_ventilator_entity.py
+++ custom_components/cvnet/lib/homeassistant/entity/cvnet_ventilator_entity.py
@@ -6,9 +6,11 @@ from homeassistant.components.fan import FanEntity, FanEntityFeature, FanEntityD
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import callback
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.util.percentage import ordered_list_item_to_percentage, percentage_to_ordered_list_item
 
 from .cvnet_entity import CvnetEntity
 
+_FAN_TYPES = [FAN_LOW, FAN_MEDIUM, FAN_HIGH]
 
 class CvnetVentilatorEntity(CvnetEntity, FanEntity):
     _wind_level = {
@@ -18,6 +20,7 @@ class CvnetVentilatorEntity(CvnetEntity, FanEntity):
     }
 
     _set_state_function: Callable[[bool, int], Coroutine]
+    _attr_custom_speed: str | None = None
 
     def __init__(self, coordinator: DataUpdateCoordinator[dict[str, Any]], entity_description: FanEntityDescription,
                  coordinator_data_key: str):
@@ -26,10 +29,8 @@ class CvnetVentilatorEntity(CvnetEntity, FanEntity):
         data = coordinator.data[coordinator_data_key]
         self._set_state_function = data[entity_description.key]["set_state_function"]
 
-        self._attr_supported_features = FanEntityFeature.PRESET_MODE | FanEntityFeature.TURN_ON | FanEntityFeature.TURN_OFF
-
-        self._attr_preset_modes = [FAN_LOW, FAN_MEDIUM, FAN_HIGH]
-        self._attr_preset_mode = data[entity_description.key]["state"]
+        self._attr_supported_features = FanEntityFeature.SET_SPEED | FanEntityFeature.TURN_ON | FanEntityFeature.TURN_OFF
+        self._attr_custom_speed = data[entity_description.key]["state"]
 
         self._attr_is_on = data[entity_description.key]["is_on"]
 
@@ -48,19 +49,42 @@ class CvnetVentilatorEntity(CvnetEntity, FanEntity):
 
         await self._set_state_function(self._attr_is_on, 0)
 
-    async def async_set_preset_mode(self, preset_mode: str) -> None:
-        self._attr_is_on = True if preset_mode != FAN_OFF else False
-        self._attr_preset_mode = preset_mode
+    async def async_set_percentage(self, percentage: int) -> None:
+        if percentage > 0:
+            wind_level = percentage_to_ordered_list_item(_FAN_TYPES, percentage) if percentage != 0 else FAN_OFF
+            self._attr_custom_speed = wind_level
+            self._attr_is_on = True
+        else:
+            wind_level = None
+            self._attr_custom_speed = None
+            self._attr_is_on = False
         self.async_write_ha_state()
 
-        await self._set_state_function(self._attr_is_on, self._wind_level[self._attr_preset_mode])
+        await self._set_state_function(self._attr_is_on, self._wind_level.get(wind_level, 0))
+
+    @property
+    def percentage(self) -> int | None:
+        if self._attr_custom_speed is None:
+            return None
+        return ordered_list_item_to_percentage(_FAN_TYPES, self._attr_custom_speed)
+
+    @property
+    def speed_count(self) -> int:
+        return len(_FAN_TYPES)
+
+    # async def async_set_preset_mode(self, preset_mode: str) -> None:
+    #     self._attr_is_on = True if preset_mode != FAN_OFF else False
+    #     self._attr_preset_mode = preset_mode
+    #     self.async_write_ha_state()
+    #
+    #     await self._set_state_function(self._attr_is_on, self._wind_level[self._attr_preset_mode])
 
     @callback
     def _handle_coordinator_update(self):
         """Handle updated data from the coordinator."""
         data = self._data
 
-        self._attr_preset_mode = data[self.entity_description.key]["state"]
+        self._attr_custom_speed = data[self.entity_description.key]["state"]
         self._attr_is_on = data[self.entity_description.key]["is_on"]
 
         super()._handle_coordinator_update()
